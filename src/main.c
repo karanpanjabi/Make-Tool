9f19d40f3e6af2af662fc613af190f78641545ad
Darth Vader <vdarth373@gmail.com>
2019-03-29 18:26:26 +0000

    test 4 added

--- src/main.c
+++ src/main.c
@@ -10,11 +10,12 @@ int main(void)
     fdt *dt;
     int r;
     char *s = "filedeptree.h";
-    char *f[2];
+    char *f[3];
 
     f[0] = s;
     f[1] = NULL;
 
+    // test 1
     dt = fdt_create(10);
     if (dt == NULL) {
         printf("Create returned NULL\n");
@@ -23,6 +24,7 @@ int main(void)
 
     fdt_print(dt);
 
+    // test 2
     r = fdt_addfile(dt, s, NULL);
     if (r != 0) {
         printf("%d: did not add file successfully\n", r);
@@ -30,7 +32,19 @@ int main(void)
 
     fdt_print(dt);
 
+    // test 3
     r = fdt_addfile(dt, "main.c", f);
+    if (r != 0) {
+        printf("%d: did not add file successfully\n", r);
+    }
+
+    fdt_print(dt);
+
+    // test 4
+    f[1] = "main.c";
+    f[2] = NULL;
+
+    r = fdt_addfile(dt, "filedeptree.c", f);
     if (r != 0) {
         printf("%d: did not add file successfully\n", r);
     }
