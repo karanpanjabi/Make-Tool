d0b49fb319934e5520e22b841dbe0d6ac5f7ec59
Darth Vader <vdarth373@gmail.com>
2019-03-31 11:31:46 +0000

    added command field to fdt

--- src/filedeptree.h
+++ src/filedeptree.h
@@ -5,16 +5,18 @@ struct filedependencytree {
     int maxfiles;
     int nfiles;
     char **files;
+    char **cmds;
     int **depgraph;
 };
 typedef struct filedependencytree fdt;
 
 
 fdt *fdt_create(int nfiles);
-int fdt_addfile(fdt *deptree, const char *fname, char **depfiles);
+int fdt_addfile(fdt *deptree, const char *fname, const char *cmd,char **depfiles);
 int fdt_delete(fdt *deptree);
-void fdt_getdependents(fdt *deptree, const char *fname, char **deps);
+void fdt_getimmediatedependants(fdt *deptree, const char *fname, char **deps);
 void fdt_print(fdt *deptree);
 int fdt_getfileidbyname(fdt *deptree, const char *name);
+char *fdt_getfilecmd(fdt *deptree, const char *fname);
 
 #endif
